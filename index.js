e7fcb2d0e43a03611260c1dd256e2f0a1f65fe70
Chase Hoselle <choselle@gmail.com>
2019-06-19 17:22:22 -0500

    updating some code

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const Joi = require('joi');
-const express = require('express');
-const genres = require('./routes/genres');
-const home = require('./routes/home');
+const Joi = require("joi");
+const express = require("express");
+const genres = require("./routes/genres");
+const home = require("./routes/home");
+const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/', home);
+app.set("view engine", "pug");
+app.set("views", "./views");
 
+app.use(express.json());
+app.use("/api/genres", genres);
+app.use("/", home);
 
 // PORT
-const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- routes/genres.js
+++ routes/genres.js
@@ -1,72 +1,76 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const genres = [
-    { id: 1, name: 'Rock' },
-    { id: 2, name: 'Country' }
-]
+  { id: 1, name: "Action" },
+  { id: 2, name: "Horror" },
+  { id: 3, name: "Romance" }
+];
 
-router.get('/', (req, res) => {
-    res.send(genres);
+router.get("/", (req, res) => {
+  res.send(genres);
 });
 
-router.get('/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
-    if (!genre) res.status(404).send('The genre with the given ID was not found.');
-    else res.send(genre);
+router.get("/:id", (req, res) => {
+  const genre = genres.find(c => c.id === parseInt(req.params.id));
+  if (!genre)
+    res.status(404).send("The genre with the given ID was not found.");
+  else res.send(genre);
 });
 
-router.post('/', (req, res) => {
-    const result = validateGenre(req.body);
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+router.post("/", (req, res) => {
+  const result = validateGenre(req.body);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
 
-    const genre = {
-        id: genre.length = 1,
-        name: req.body.name
-    };
-    genres.push(genre);
-    res.send(genre);
+  const genre = {
+    id: (genre.length = 1),
+    name: req.body.name
+  };
+  genres.push(genre);
+  res.send(genre);
 });
 
-router.put(':id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
-    if (!genre) {
-        res.status(404).send('The genre with the given ID was not found.');
-        return;
-    }
+router.put("/:id", (req, res) => {
+  const genre = genres.find(c => c.id === parseInt(req.params.id));
+  if (!genre) {
+    res.status(404).send("The genre with the given ID was not found.");
+    return;
+  }
 
-    const result = validateGenre(req.body);
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+  const result = validateGenre(req.body);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
 
-    genre.name = req.body.name;
-    res.send(genre);
+  genre.name = req.body.name;
+  res.send(genre);
 });
 
-router.delete('/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
-    if (!genre) {
-        res.status(404).send('The genre with the given ID was not found');
-        return;
-    }
+router.delete("/:id", (req, res) => {
+  const genre = genres.find(c => c.id === parseInt(req.params.id));
+  if (!genre) {
+    res.status(404).send("The genre with the given ID was not found");
+    return;
+  }
 
-    const index = genres.indexOf(genre);
-    genres.splice(index, 1);
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
 
-    res.send(course);
+  res.send(course);
 });
 
 function validateGenre(genre) {
-    const schema = {
-        name: Joi.string().min(3).require()
-    };
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .require()
+  };
 
-    return Joi.validate(course, schema);
+  return Joi.validate(course, schema);
 }
 
 module.exports = router;
